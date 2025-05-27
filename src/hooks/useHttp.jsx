100ac05aa294a752c32eba756093b55ff4be6535
TedGC <rhkdtn2056@gmail.com>
2025-05-27 17:28:43 +0900

    update

--- src/hooks/useHttp.jsx
+++ src/hooks/useHttp.jsx
@@ -16,4 +16,43 @@ async function sendHttpRequest(url, config) {
 }
 
 
-export default function use
+export default function useHttp(url, config, initialData) {
+
+
+    const [data, setData] = useState(initialData)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState()
+
+    function clearData() {
+        setData(initialData)
+    }
+
+    const sendRequest = useCallback(
+        async function sendRequest(data) {
+            setIsLoading(true)
+
+            try {
+                const response = await sendHttpRequest(url, { ...config, body: data })
+                setData(resData)
+            }
+            catch (error) {
+                setError(error.message || 'something went wrong')
+            }
+            setIsLoading(false)
+                , [url, config]
+        }
+    )
+    useEffect(() => {
+        if ((config && (config.method === 'GET' || !config.method)) || !config) {
+            sendRequest()
+        }
+    }, [config, sendRequest])
+
+    return (
+        isLoading,
+        data,
+        error,
+        sendRequest,
+        clearData
+    )
+}
